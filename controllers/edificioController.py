6290087e807b65c6be3241142947e4a230e6be9d
mimirandaipvc <mimiranda@ipvc.pt>
2022-05-29 02:59:32 +0100

    Redis

--- controllers/edificioController.py
+++ controllers/edificioController.py
@@ -1,10 +1,18 @@
 from models.edificio import Edificio
 from config.mongodb import colEdificio
+from config.redisdb import r
 from schemas.edificioSchema import edificioEntity, edificiosEntity
 
 
 async def find_all_edificios_mongodb():
     return edificiosEntity(colEdificio.find())
+
+
+async def find_all_edificios_redis():
+    edificios = []
+    for key in r.keys():
+        edificios.append(r.hgetall(key))
+    return edificios
 
 
 # cria o edificio e retorna o id inserido
@@ -19,6 +27,22 @@ def create_edificio_mongodb(newName, newType, newPrice):
             print("Edificio already Inserted")
         else:
             print('Upserted edificio with id:', created.upserted_id)
+    except Exception as e:
+        return "Ocorreu algum erro ao inserir Edificio"
+    return "Edificio Upserted"
+
+
+def create_edificio_redis(newName, newType, newPrice):
+    try:
+        valueID = newName + "-" + newType + "-" + newPrice
+        edificio = {"name": newName, "type": newType, "price": newPrice}
+        a = r.hkeys(valueID)
+        if (len(a) > 0):
+            r.hmset(valueID, edificio)
+            print('Updated edificio with id:', valueID)
+        else:
+            r.hmset(valueID, edificio)
+            print('Edificio inserted')
     except Exception as e:
         return "Ocorreu algum erro ao inserir Edificio"
     return "Edificio Upserted"
--- routes/edificiosRouter.py
+++ routes/edificiosRouter.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from controllers.edificioController import find_all_edificios_mongodb, create_edificio_mongodb
+from controllers.edificioController import find_all_edificios_mongodb, create_edificio_mongodb, create_edificio_redis, \
+    find_all_edificios_redis
 
 from models.edificio import Edificio
 
@@ -7,13 +8,24 @@ from models.edificio import Edificio
 edificioRouter = APIRouter()
 
 
-@edificioRouter.get("/api/edificios")
+@edificioRouter.get("/api/edificios_mongo")
 async def get_all_edificios_mongodb():
     return await find_all_edificios_mongodb()
 
-@edificioRouter.post("/api/edificios")
+
+@edificioRouter.get("/api/edificios_redis")
+async def get_all_edificios_redis():
+    return await find_all_edificios_redis()
+
+
+@edificioRouter.post("/api/edificios_mongo")
 async def post_edificio_mongodb(edificio:Edificio):
     return await create_edificio_mongodb(edificio)
+
+
+@edificioRouter.post("/api/edificios_redis")
+async def post_edificio_redis(edificio:Edificio):
+    return await create_edificio_redis(edificio)
 
 
 
