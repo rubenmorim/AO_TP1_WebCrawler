98ec4673dde9157c7d0c8b2db32dde28cc64c458
Ruben Morim <rubenmorim17@gmail.com>
2022-05-30 22:25:50 +0100

    stable 0.3 - added insertion localizacao

--- controllers/localizacaoController.py
+++ controllers/localizacaoController.py
@@ -11,10 +11,6 @@ def create_localizacao_mongodb(newLocalizacao, newConcelho):
         # upsert with replace_one
         created = colLocalizacao.replace_one(
             {"localizacao": localizacaoObject.localizacao,"concelho": localizacaoObject.concelho}, dict(localizacaoObject), upsert=True)
-        if created.matched_count == 1:
-            print("Localizacao already Inserted")
-        else:
-            print('Localizacao : ', created.upserted_id)
 
         if created.upserted_id is not None:
             localizacaoID = created.upserted_id
