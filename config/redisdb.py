83dad55af70a2b003ccdb8e5806654612e7d861b
mimirandaipvc <mimiranda@ipvc.pt>
2022-05-31 23:33:04 +0100

    redis v0.6

--- config/redisdb.py
+++ config/redisdb.py
@@ -1,9 +1,28 @@
 import redis
 
-r = redis.StrictRedis(
+redificio = redis.StrictRedis(
    host='127.0.0.1',
    port=6379,
    password='',
+   db=1,
+   charset="utf-8",
+   decode_responses=True
+)
+
+rlocalizacao = redis.StrictRedis(
+   host='127.0.0.1',
+   port=6379,
+   password='',
+   db=2,
+   charset="utf-8",
+   decode_responses=True
+)
+
+rvendedor = redis.StrictRedis(
+   host='127.0.0.1',
+   port=6379,
+   password='',
+   db=3,
    charset="utf-8",
    decode_responses=True
 )
